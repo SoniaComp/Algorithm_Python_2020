7d010b84d9b7fc4f91b11a0dd6f814c423f2b272
soniacomp <syparkehsc@naver.com>
2020-06-11 00:11:18 +0900

    dfs

--- BaekJoon/BFS_2589.py
+++ BaekJoon/BFS_2589.py
@@ -1,3 +1,7 @@
+# BFS는 최단거리를 찾자마자 종료할 수 있고,
+# DFS는 모든 거리를 다 찾아야 함
+
+from collections import deque
 import sys
 input = sys.stdin.readline
 
@@ -7,24 +11,31 @@ maze = [list(input().strip()) for _ in range(N)]
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
-maxcount = 0
+q = deque()
+maxCount = 0
 
 
-def dfs(x, y, count):
-    global maxcount
-    for i in range(4):
-        if x+dx[i] > N-1 or x+dx[i] < 0 or y+dy[i] < 0 or y+dy[i] > M-1:
-            continue
-        if(maze[x+dx[i]][y+dy[i]] == 'L'):
-            maze[x+dx[i]][y+dy[i]] = 'W'
-            count += 1
-            if(count > maxcount):
-                maxcount = count
-            dfs(x+dx[i], y+dy[i], count)
-        else:
-            continue
-    return count
+def bfs(x, y):
+    cnt = 0
+    q.append([x, y])
+    c = [[0]*M for _ in range(N)]
+    c[x][y] = 1
+    maxN = 0
+    while q:
+        [x, y] = q.popleft()
+        for i in range(4):
+            Nx, Ny = x+dx[i], y+dy[i]
+            if 0 < Nx < N and 0 < Ny < M and maze[Nx][Ny] == 'L':
+                q.append([Nx, Ny])
+                maze[Nx][Ny] = 'W'
+                c[Nx][Ny] = c[x][y]+1
+                maxN = max(c[Nx][Ny], maxN)
+    return maxN - 1
 
 
-dfs(0, 0, 0)
-print(maxcount)
+for i in range(N):
+    for j in range(M):
+        if maze[i][j] == 'L':
+            maxCount = (maxCount, bfs(i, j))
+
+print(maxCount)
